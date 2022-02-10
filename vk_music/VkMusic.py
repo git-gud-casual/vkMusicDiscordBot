3679320f6219f2884c6f68ae65fb2c7ae13d5e92
admin <popasena2@ya.ru>
2022-02-10 11:38:04 +0300

    Fix

--- vk_music/VkMusic.py
+++ vk_music/VkMusic.py
@@ -26,10 +26,13 @@ class VkMusic(commands.Cog):
             channel: discord.VoiceChannel = ctx.message.author.voice.channel
             voice = get(self.bot.voice_clients, guild=ctx.guild)
 
+            print(voice.channel.id, channel.id)
             if voice and voice.is_connected() and voice.channel != channel:
                 self.queues.remove(voice)
                 await voice.disconnect()
-            await channel.connect()
+
+            if not voice:
+                await channel.connect()
         else:
             embed = discord.Embed(title='Error', color=discord.Color.red())
             embed.description = 'You are not in voice channel'
